fe7a33851b72d0e468b62b0fed8a41c952fd9245
KashtahApp <Alothman.Me@gmail.com>
2023-04-25 14:25:52 +0400

    lot of switch statments

---
+++ lowering-switch-code.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoweringC_
+{
+    internal class lowering_switch_code
+    {
+    }
+}
