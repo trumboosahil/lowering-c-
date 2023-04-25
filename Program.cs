f600a3cc9ce95d1301f81a788bbadfaf097c0f25
KashtahApp <Alothman.Me@gmail.com>
2023-04-25 13:33:32 +0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var items = new[] { "shafi", "houd", "trumboo" };
+foreach(var item in items)
+{
+    Console.Out.WriteLine(item);
+}
+
+for (int i = 0; i < items.Length; i++)
+{
+    Console.Out.WriteLine(items[i]);
+}
+int i1 = 0;
+while(i1 < items.Length)
+{
+    Console.Out.WriteLine(items[i1]);
+    i1++;
+}
