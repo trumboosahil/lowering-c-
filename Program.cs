9b9701f423736b6830d47264908c8033c6ed0495
KashtahApp <Alothman.Me@gmail.com>
2023-04-25 15:07:55 +0400

    record

--- Program.cs
+++ Program.cs
@@ -4,4 +4,8 @@ using LoweringC_;
 Console.WriteLine("Lowering C# example!");
 lowering_switch lowering_Switch = new lowering_switch();
 
-lowering_Switch.RunSwitch();
+//lowering_Switch.RunSwitch();
+Student student1 = new Student("shafi");
+Student student2 = new Student("shafi");
+Console.WriteLine(student2 == student1);
+
---
+++ Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoweringC_
+{
+    record Student
+    {
+        public Student(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; init; }
+    }
+}
